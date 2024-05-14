0cacc5d5259e2ce280af6d70158f608ac60188a7
VeldinS <veldinsalcinovic123@gmail.com>
2024-05-14 18:57:31 +0200

    Validating User Input With Help Of The useFormState Hook

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,13 +1,33 @@
 import { storePost } from '@/lib/posts';
 import {redirect} from "next/navigation";
 import FormSubmit from "@/components/form-submit";
+import { useActionState } from 'react';
 
 export default function NewPostPage() {
+
     async function createPost(formData) {
         "use server";
         const title = formData.get('title');
         const image = formData.get('image');
         const content = formData.get('content');
+
+        let errors = [];
+
+        if(!title || title.trim().length === 0){
+            errors.push('Title is required!');
+        }
+
+        if(!content || content.trim().length === 0){
+            errors.push('Content is required!');
+        }
+
+        if(!image){
+            errors.push('Image is required!');
+        }
+
+        if(errors.length >0){
+            return { errors }
+        }
 
         await storePost({
             imageUrl: '',
@@ -19,10 +39,12 @@ export default function NewPostPage() {
         redirect('/feed')
     }
 
+    const [state, formAction] = useActionState(createPost, {});
+
     return (
         <>
             <h1>Create a new post</h1>
-            <form action={createPost}>
+            <form action={formAction}>
                 <p className="form-control">
                     <label htmlFor="title">Title</label>
                     <input type="text" id="title" name="title" />
